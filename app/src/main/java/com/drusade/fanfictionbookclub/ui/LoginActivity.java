a786efb5cf3fbdc2bfb577dff2bf6cd3a34b174a
DruSadeMumba <drusademumba@gmail.com>
2022-06-18 23:05:26 +0300

    Add loginactivity logic

--- app/src/main/java/com/drusade/fanfictionbookclub/ui/LoginActivity.java
+++ app/src/main/java/com/drusade/fanfictionbookclub/ui/LoginActivity.java
@@ -1,16 +1,122 @@
 package com.drusade.fanfictionbookclub.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.drusade.fanfictionbookclub.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    public static final String TAG = LoginActivity.class.getSimpleName();
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.userLoginEmailEditText) EditText mUserLoginEmailEditText;
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.userLoginPasswordEditText) EditText mUserLoginPasswordEditText;
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.logInButton) Button mLoginButton;
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.textViewTitle) TextView mTextViewTitle;
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.createAccountTextView) TextView mCreateAccountTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+
+        mCreateAccountTextView.setOnClickListener(this);
+        mLoginButton.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mCreateAccountTextView) {
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        if (v == mLoginButton) {
+            loginWithPassword();
+        }
+    }
+
+    private void loginWithPassword() {
+        String email = mUserLoginEmailEditText.getText().toString().trim();
+        String password = mUserLoginPasswordEditText.getText().toString().trim();
+        if (email.equals("")) {
+            mUserLoginEmailEditText.setError("Please enter your email");
+            return;
+        }
+        if (password.equals("")) {
+            mUserLoginPasswordEditText.setError("Password cannot be blank");
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
 }
